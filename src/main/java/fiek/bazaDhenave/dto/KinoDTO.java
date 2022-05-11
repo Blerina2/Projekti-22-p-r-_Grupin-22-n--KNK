bf0951563aa9643cd3555f8c2990e6b8ef1b598e
Arbreshass <arbresha.stublla@student.uni-pr.edu>
2022-05-11 13:51:49 +0300

    Vazhdimi i dy klasava DTO

---
+++ src/main/java/fiek/bazaDhenave/dto/KinoDTO.java
@@ -0,0 +1,91 @@
+package kinema.fiek.bazaDhenave.dto;
+
+import javafx.scene.control.Alert;
+import javafx.stage.Window;
+import kinema.fiek.bazaDhenave.LidhjaBazesDhenave;
+import kinema.fiek.entity.Kino;
+import kinema.fiek.ndihmes.PopUpInformacion;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class KinoDTO {
+
+    private static Connection connection;
+
+    static {
+        LidhjaBazesDhenave dbc = LidhjaBazesDhenave.getDatabaseConnection();
+        connection = dbc.getConnection();
+    }
+
+    public static boolean akutalizoKinon(int kinoId, String karriageList, int karraiageveReservuar, Window window) {
+        try {
+            PreparedStatement ps;
+            String query = "update kino Set KARRIGE_LIST = ?, KARRIAGEVE_RESERVUAR = ? where ID=?";
+            ps = connection.prepareStatement(query);
+            ps.setString(1, karriageList);
+            ps.setString(2, String.valueOf(karraiageveReservuar));
+            ps.setString(3, String.valueOf(kinoId));
+            if (ps.executeUpdate() > 0) {
+//                PopUpInformacion.showAlert(Alert.AlertType.INFORMATION, window, "Information",
+//                        "Ju jeni regjistruar me sukses ne kino");
+            } else {
+                PopUpInformacion.showAlert(Alert.AlertType.ERROR, window, "Gabim",
+                        "Dicka shkoi keq.");
+                return false;
+            }
+
+        } catch (SQLException ex) {
+            PopUpInformacion.showAlert(Alert.AlertType.ERROR, window, "Gabim",
+                    "Dicka shkoi keq.");
+            return false;
+        }
+        return true;
+    }
+
+
+    public static List<Kino> getALlKinos() {
+        List<Kino> kinos = new ArrayList<>();
+        try {
+            String query = "select ID, KINO_NAME,SASIA_KARRIAGEVE, KARRIGE_LIST, KARRIAGEVE_RESERVUAR from Kino";
+            PreparedStatement ps = connection.prepareStatement(query);
+            ResultSet resultSet = ps.executeQuery();
+            ResultSetMetaData resultSetMetaData = resultSet.getMetaData();
+            final int columnCount = resultSetMetaData.getColumnCount();
+
+            List<Object[]> objects = new ArrayList<>();
+
+            while (resultSet.next()) {
+                Object[] values = new Object[columnCount];
+                for (int i = 1; i <= columnCount; i++) {
+                    values[i - 1] = resultSet.getObject(i);
+                }
+                objects.add(values);
+            }
+
+            return convertoNeObjekteKino(objects);
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return kinos;
+    }
+
+    private static List<Kino> convertoNeObjekteKino(List<Object[]> objects) {
+        List<Kino> kinos = new ArrayList<>();
+
+        for (Object[] object : objects) {
+            Kino kino = new Kino();
+            kino.setKinoID((int) object[0]);
+            kino.setKinoName((String) object[1]);
+            kino.setSasiaKarriageve((int) object[2]);
+            kino.setKarrigeList((String) object[3]);
+            kino.setKarriageveReservuar((int) object[4]);
+            // shto filmi ne list
+            kinos.add(kino);
+        }
+        return kinos;
+    }
+}
---
+++ src/main/java/fiek/bazaDhenave/dto/UserDTO.java
@@ -0,0 +1,107 @@
+package kinema.fiek.bazaDhenave.dto;
+
+import javafx.scene.control.Alert;
+import javafx.stage.Window;
+import kinema.fiek.bazaDhenave.LidhjaBazesDhenave;
+import kinema.fiek.entity.Film;
+import kinema.fiek.entity.Kino;
+import kinema.fiek.entity.Rezervim;
+import kinema.fiek.entity.User;
+import kinema.fiek.ndihmes.PopUpInformacion;
+import kinema.fiek.ndihmes.Session;
+import kinema.fiek.ndihmes.UserNdihmes;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class UserDto {
+
+    private static Connection connection;
+
+    static {
+        LidhjaBazesDhenave dbc = LidhjaBazesDhenave.getDatabaseConnection();
+        connection = dbc.getConnection();
+    }
+
+
+    public static User getUser(String username, String password, Window window) {
+
+        try {
+            String query = "select  ID, FIRST_NAME, LAST_NAME, EMAIL, USER_NAME, PASSWORD, USER_ROLE from users WHERE user_name = ? and password = ?";
+            PreparedStatement preparedStatement;
+            ResultSet resultSet;
+            preparedStatement = connection.prepareStatement(query);
+            preparedStatement.setString(1, username);
+            preparedStatement.setString(2, password);
+
+            resultSet = preparedStatement.executeQuery();
+            ResultSetMetaData resultSetMetaData = resultSet.getMetaData();
+            final int columnCount = resultSetMetaData.getColumnCount();
+
+
+            if (resultSet.next()) {
+                List<Object[]> objects = new ArrayList<>();
+                Object[] values = new Object[columnCount];
+                for (int i = 1; i <= columnCount; i++) {
+                    values[i - 1] = resultSet.getObject(i);
+                }
+                objects.add(values);
+
+                List<User> users = UserNdihmes.convertoNeObjekteTeUser(objects);
+                User user = users.get(0);
+                // Gjate perdorimit te appit klient eshte i nevojshme te perdoret
+                Session.setUser(user);
+                // Gjate perdorimit te appit klienti i ka filma gati
+                List<Film> films = FilmDTO.loadFilmatPrejDB();
+                Session.setFilmList(films);
+                List<Kino> kinos = KinoDTO.getALlKinos();
+                Session.setKinoList(kinos);
+                return user;
+            }
+        } catch (SQLException ex) {
+            PopUpInformacion.showAlert(Alert.AlertType.ERROR, window, "Gabim",
+                    "Problem me bazen e dhenave");
+        }
+
+        return null;
+    }
+
+    public static User getRezervimet(int userId, Window window) {
+
+        try {
+            String query = "select ka.NUMRI_KARRIGES, ka.DATA_REZERVIMIT, ka.KOHA_FILMIT, ka.ORA_FILMIT, ki.KINO_NAME, ki.ID\n" +
+                    "from users u join karrige ka on u.ID = ka.USER_ID join movies m on ka.KINO_ID = m.KINO_ID join kino ki on ki.ID =ka.KINO_ID where u.ID=? order by ka.KINO_ID asc;";
+            PreparedStatement preparedStatement;
+            ResultSet resultSet;
+            preparedStatement = connection.prepareStatement(query);
+            preparedStatement.setString(1, String.valueOf(userId));
+
+            resultSet = preparedStatement.executeQuery();
+            ResultSetMetaData resultSetMetaData = resultSet.getMetaData();
+            final int columnCount = resultSetMetaData.getColumnCount();
+
+            List<Rezervim> rezervimList = new ArrayList<>();
+
+            while (resultSet.next()) {
+                List<Object[]> objects = new ArrayList<>();
+                Object[] values = new Object[columnCount];
+                for (int i = 1; i <= columnCount; i++) {
+                    values[i - 1] = resultSet.getObject(i);
+                }
+                objects.add(values);
+
+                rezervimList.add(UserNdihmes.converToRezervimetObjekt(objects));
+            }
+            User user = Session.getUser();
+            user.setRezervim(rezervimList);
+            // Gjate perdorimit te appit klient eshte i nevojshme te perdoret
+            Session.setUser(user);
+            return user;
+        } catch (SQLException ex) {
+            PopUpInformacion.showAlert(Alert.AlertType.ERROR, window, "Gabim",
+                    "Problem me bazen e dhenave");
+        }
+        return null;
+    }
+}
