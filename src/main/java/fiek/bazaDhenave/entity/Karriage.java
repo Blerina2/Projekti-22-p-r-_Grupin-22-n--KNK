a2b81b8771e158769442c15d7bb73bceb95883ea
BlendGashi <94861491+BlendGashi@users.noreply.github.com>
2022-05-11 13:09:41 +0200

    Vazhdimi i klasave ne entity

---
+++ src/main/java/fiek/bazaDhenave/entity/Karriage.java
@@ -0,0 +1,59 @@
+package kinema.fiek.entity;
+
+public class Karriage {
+
+    private String numriKarriges;
+    private int userId;
+    private String dataRezervimit;
+    private String kohaFilmit;
+    private String oraFilmit;
+    private int kinoId;
+
+    public String getNumriKarriges() {
+        return numriKarriges;
+    }
+
+    public void setNumriKarriges(String numriKarriges) {
+        this.numriKarriges = numriKarriges;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getDataRezervimit() {
+        return dataRezervimit;
+    }
+
+    public void setDataRezervimit(String dataRezervimit) {
+        this.dataRezervimit = dataRezervimit;
+    }
+
+    public String getKohaFilmit() {
+        return kohaFilmit;
+    }
+
+    public void setKohaFilmit(String kohaFilmit) {
+        this.kohaFilmit = kohaFilmit;
+    }
+
+    public String getOraFilmit() {
+        return oraFilmit;
+    }
+
+    public void setOraFilmit(String oraFilmit) {
+        this.oraFilmit = oraFilmit;
+    }
+
+    public int getKinoId() {
+        return kinoId;
+    }
+
+    public void setKinoId(int kinoId) {
+        this.kinoId = kinoId;
+    }
+}
---
+++ src/main/java/fiek/bazaDhenave/entity/Kino.java
@@ -0,0 +1,52 @@
+package kinema.fiek.entity;
+
+public class Kino {
+    private int kinoID;
+    private String kinoName;
+    private String karrigeList;
+
+    private int karriageveReservuar;
+
+
+    private int sasiaKarriageve;
+
+    public int getSasiaKarriageve() {
+        return sasiaKarriageve;
+    }
+
+    public void setSasiaKarriageve(int sasiaKarriageve) {
+        this.sasiaKarriageve = sasiaKarriageve;
+    }
+
+    public int getKarriageveReservuar() {
+        return karriageveReservuar;
+    }
+
+    public void setKarriageveReservuar(int karriageveReservuar) {
+        this.karriageveReservuar = karriageveReservuar;
+    }
+
+    public int getKinoID() {
+        return kinoID;
+    }
+
+    public void setKinoID(int kinoID) {
+        this.kinoID = kinoID;
+    }
+
+    public String getKinoName() {
+        return kinoName;
+    }
+
+    public void setKinoName(String kinoName) {
+        this.kinoName = kinoName;
+    }
+
+    public String getKarrigeList() {
+        return karrigeList;
+    }
+
+    public void setKarrigeList(String karrigeList) {
+        this.karrigeList = karrigeList;
+    }
+}
