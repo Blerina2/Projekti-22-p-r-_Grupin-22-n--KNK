99d9a247e0c7bcc60b49b4fe46b006dd2678d9c4
blertaujkani <94452970+blertaujkani@users.noreply.github.com>
2022-05-11 12:34:11 +0200

    Krijimi i kalsave per karikat dhe per kinemat

---
+++ src/main/java/fiek/bazaDhenave/dto/FilmDTO.java
@@ -0,0 +1,90 @@
+package kinema.fiek.bazaDhenave.dto;
+
+import javafx.scene.image.Image;
+import kinema.fiek.bazaDhenave.LidhjaBazesDhenave;
+import kinema.fiek.entity.Film;
+
+import java.io.ByteArrayInputStream;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class FilmDTO {
+
+
+    private static Connection connection;
+
+    static {
+        LidhjaBazesDhenave dbc = LidhjaBazesDhenave.getDatabaseConnection();
+        connection = dbc.getConnection();
+    }
+
+    /**
+     * @param objects qe perban reshtat e tables movie
+     * @return list me objekti te filmave.
+     */
+    public static List<Film> convertoNeObjekteFilmit(List<Object[]> objects) {
+        List<Film> films = new ArrayList<>();
+
+        for (Object[] object : objects) {
+            Film film = new Film();
+            film.setId((int) object[0]);
+            film.setTitle((String) object[1]);
+            film.setPershkrimi((String) object[2]);
+            film.setTrailer((String) object[3]);
+            film.setStartdate((String) object[4]);
+            film.setEndate((String) object[5]);
+            film.setTime1((String) object[6]);
+            film.setTime2((String) object[7]);
+
+            // convertje byte array ne objekt te image
+            byte[] bytes = (byte[]) object[8];
+            ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(bytes);
+            Image image = new Image(byteArrayInputStream);
+            film.setIMAGE(image);
+
+            film.setKinoID((int) object[9]);
+
+            // shto filmi ne list
+            films.add(film);
+        }
+        // ne fund sortoj permese titles (a-z)
+        return films.stream().sorted(Comparator.comparing(Film::getTitle)).collect(Collectors.toList());
+    }
+
+    /**
+     * load all films from database.
+     *
+     * @return a list of film object.
+     */
+    public static List<Film> loadFilmatPrejDB() {
+        List<Film> films = new ArrayList<>();
+        try {
+            PreparedStatement ps;
+            String query = "select ID, TITLE, PERSHKRIMI, TRAILER, STARTDATE, ENDATE, TIME1, TIME2, IMAGE, KINO_ID from movies";
+            ps = connection.prepareStatement(query);
+            ResultSet resultSet = ps.executeQuery();
+            ResultSetMetaData resultSetMetaData = resultSet.getMetaData();
+            final int columnCount = resultSetMetaData.getColumnCount();
+
+            List<Object[]> objects = new ArrayList<>();
+
+            while (resultSet.next()) {
+                Object[] values = new Object[columnCount];
+                for (int i = 1; i <= columnCount; i++) {
+                    values[i - 1] = resultSet.getObject(i);
+                }
+                objects.add(values);
+            }
+
+            return FilmDTO.convertoNeObjekteFilmit(objects);
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return films;
+    }
+}
---
+++ src/main/java/fiek/bazaDhenave/dto/KarrigaDAO.java
@@ -0,0 +1,54 @@
+package kinema.fiek.bazaDhenave.dto;
+
+import javafx.scene.control.Alert;
+import javafx.stage.Window;
+import kinema.fiek.bazaDhenave.LidhjaBazesDhenave;
+import kinema.fiek.ndihmes.PopUpInformacion;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+
+/**
+ * query ne tablen KINO dhe KARRIGE
+ * DAO := Data Access Object
+ */
+public class KarrigaDAO {
+
+    private static Connection connection;
+
+    static {
+        LidhjaBazesDhenave dbc = LidhjaBazesDhenave.getDatabaseConnection();
+        connection = dbc.getConnection();
+    }
+
+
+    public static int rezervojKarragen(String karriage, int userId, String dataRezervimit, String kohaFilmit, String oraFilmit, String kino_id, Window window) {
+        try {
+            PreparedStatement ps;
+            String query = "insert into karrige(NUMRI_KARRIGES, USER_ID, DATA_REZERVIMIT, KOHA_FILMIT, ORA_FILMIT, KINO_ID) VALUES (?,?,?,?,?,?)";
+            ps = connection.prepareStatement(query);
+            ps.setString(1, karriage);
+            ps.setString(2, String.valueOf(userId));
+            ps.setString(3, dataRezervimit);
+            ps.setString(4, kohaFilmit);
+            ps.setString(5, oraFilmit);
+            ps.setString(6, kino_id);
+            if (ps.executeUpdate() > 0) {
+//                PopUpInformacion.showAlert(Alert.AlertType.INFORMATION, window, "Information",
+//                        "Ju jeni regjistruar me sukses karriagen");
+            } else {
+                PopUpInformacion.showAlert(Alert.AlertType.ERROR, window, "Gabim",
+                        "Dicka shkoi keq.");
+                return 0;
+            }
+
+        } catch (SQLException ex) {
+            PopUpInformacion.showAlert(Alert.AlertType.ERROR, window, "Gabim",
+                    "Dicka shkoi keq." + ex.getMessage());
+            return 0;
+        }
+        return 1;
+    }
+}
