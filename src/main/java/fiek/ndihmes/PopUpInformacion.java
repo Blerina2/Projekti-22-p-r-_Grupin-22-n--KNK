0c8cafdcedbfd9421e9d06d240e4640346627a52
dionberisha23 <94796934+dionberisha23@users.noreply.github.com>
2022-05-14 19:11:54 +0200

    Update PopUpInformacion.java
    Krijimi i klases PopUpInformacion

--- src/main/java/fiek/ndihmes/PopUpInformacion.java
+++ src/main/java/fiek/ndihmes/PopUpInformacion.java
@@ -1,4 +1,33 @@
-package fiek.ndihmes;
+package kinema.fiek.ndihmes;
+
+
+import javafx.scene.control.Alert;
+import javafx.stage.Window;
+import javafx.util.Duration;
+import org.controlsfx.control.Notifications;
 
 public class PopUpInformacion {
+
+    public static void showAlert(Alert.AlertType alertType, Window owner, String title, String message) {
+
+
+        if (alertType.name() == null ? alertType.INFORMATION.name() == null : alertType.name().equals(alertType.INFORMATION.name())) {
+            Notifications.create()
+              .title(title)
+                    .text(message).hideAfter(Duration.seconds(10))
+                    .showInformation();
+          } else if (alertType.name() == null ? Alert.AlertType.ERROR.name() == null : alertType.name().equals(Alert.AlertType.ERROR.name())) {
+            Notifications.create()
+                    .title(title)
+                    .text(message).hideAfter(Duration.seconds(10))
+                    .showError();
+        }
+      else if (alertType.name() == null ? Alert.AlertType.CONFIRMATION.name() == null : alertType.name().equals(Alert.AlertType.CONFIRMATION.name())) {
+            Notifications.create()
+                    .title(title)
+                    .owner(owner)
+                    .text(message).hideAfter(Duration.seconds(10))
+                    .showConfirm();
+        }
+    }
 }
