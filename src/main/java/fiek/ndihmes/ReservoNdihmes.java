399224e1121541f3be257ee940c4ba4171bcd8a7
dionberisha23 <94796934+dionberisha23@users.noreply.github.com>
2022-05-14 19:13:04 +0200

    Update ReservoNdihmes.java
    Krijimi i klases Rezervo/Ndihmes

--- src/main/java/fiek/ndihmes/ReservoNdihmes.java
+++ src/main/java/fiek/ndihmes/ReservoNdihmes.java
@@ -1,4 +1,29 @@
-package fiek.ndihmes;
+package kinema.fiek.ndihmes;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import kinema.fiek.entity.Film;
+
+import java.util.List;
 
 public class ReservoNdihmes {
+
+    public static ObservableList<String> getFilmatPerComboBox() {
+        List<Film> filmList = Session.getFilmList();
+        // me duhet sepse javafx ComboBox kerkon qe elemente te jen ne ObservableList
+        ObservableList<String> filmTitles = FXCollections.observableArrayList();
+
+        for (Film film : filmList) {
+            filmTitles.add(film.getTitle());
+        }
+        return filmTitles;
+    }
+
+    public static ObservableList<String> getKohatFilmit(Film film) {
+        // me duhet sepse javafx ComboBox kerkon qe elemente te jen ne ObservableList
+        ObservableList<String> filmTitles = FXCollections.observableArrayList();
+        filmTitles.add(film.getTime1());
+        filmTitles.add(film.getTime2());
+        return filmTitles;
+    }
 }
