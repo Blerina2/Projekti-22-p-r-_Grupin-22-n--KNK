e9112f421e449f2d445d282a284e2f6bb4fb9d57
Blerina2 <blerina.beka@student.uni-pr.edu>
2022-05-19 13:48:27 +0200

    Krijimi i folderva per pamje vizuale si dhe krijimi i allfilms.css

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -9,7 +9,7 @@ module com.example.blerinakinema {
     requires org.kordamp.ikonli.javafx;
     requires org.kordamp.bootstrapfx.core;
     requires eu.hansolo.tilesfx;
-     requires com.almasb.fxgl.all;
+    requires com.almasb.fxgl.all;
     requires java.logging;
     requires java.sql;
     requires java.desktop;
